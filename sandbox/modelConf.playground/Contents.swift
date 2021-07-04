2fbfae5f092ae900da4d09605f346f5ab416fc48
YE JIN LEE <84134902+yeah-zin@users.noreply.github.com>
2021-07-04 17:37:01 +0900

    test model

--- sandbox/modelConf.playground/Contents.swift
+++ sandbox/modelConf.playground/Contents.swift
@@ -14,5 +14,3 @@ var challenges: [Challenge] = [
     Challenge(title: "필기시험 4주 뿌셔", period: 28, desc: "4주간 준비할 수 있는 필기시험 공부하는 과정", category: "자격증 공부", authenticationMethod: "시중에 판매하는 한달 과정의 문제집 등을 1day풀고 사진찍어 인증하기"),
     Challenge(title: "체지방 1kg 빼기", period: 14, desc: "근손실없이 건강하게 체지방 빼기", category: "운동", authenticationMethod: "매일 근력운동 1시간 + 유산소 1시간 하기")
 ]
-
-
