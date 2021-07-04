c730d63f21efbe6ea20ee639130e2bb6fc83e0f0
YE JIN LEE <84134902+yeah-zin@users.noreply.github.com>
2021-07-04 15:59:41 +0900

    model conf

---
+++ sandbox/modelConf.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+struct Challenge {
+    var title : String
+    var period : Int
+    var desc : String
+    var category : String
+    var authenticationMethod : String
+}
+
+let chall1 = Challenge(title: "Front-end", period: 30, desc: "30일 동안 html, css, JS에 대한 개념을 잡아보자", category: "Coding", authenticationMethod: "Dream Coding 무료 동영상 강의를 듣고 해당 강의에서 작성한 코드를 캡쳐하여 인증")
+
+
