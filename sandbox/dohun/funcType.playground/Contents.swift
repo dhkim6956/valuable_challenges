7ca1ca714c861320099a7480300c5e4f9d174e73
dhkim6956 <84961914+dhkim6956@users.noreply.github.com>
2021-07-25 14:44:05 +0900

    funcType

---
+++ sandbox/dohun/funcType.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+func intAndDouble(_ getInt: Int, _ getDouble: Double) -> Int {
+    return 0
+}
+
+let iAD = intAndDouble
+
+func stringArray(_ getStringArray: [String]) -> Int {
+    return 0
+}
+
+let sA = stringArray
+
+func stringAndInt(_ getString: String, getInt: Int) -> [String: Int] {
+    return ["String0":0, "String1":1]
+}
+
+let sI = stringAndInt
+
