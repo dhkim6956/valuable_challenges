22d601ac7dd1e0b0c0b8367fac73dbcbcb0fa538
YE JIN LEE <84134902+yeah-zin@users.noreply.github.com>
2021-07-25 15:25:14 +0900

    fix

--- sandbox/yeah-zin/ClosureExample.playground/Contents.swift
+++ sandbox/yeah-zin/ClosureExample.playground/Contents.swift
@@ -27,6 +27,42 @@ func stringInt(_ arg1:String, _ arg2:Int) -> [String: Int] {
 let func3 = stringInt
 func3("hello", 3)
 
+func addTwoNum(_ arg1: Int, _ arg2:Int) -> Int {
+    return 0
+}
 
 
+let addTwoclosure = {(arg1: Int, arg2: Int) -> Int in
+    return arg1 + arg2
+}
 
+let addTwoclosure2 = {(arg1, arg2) -> Int in
+    return arg1 + arg2
+}
+
+let addTwoclosure3 = {(arg1, arg2) -> Int in
+    arg1 + arg2
+}
+
+let addTwoclosure4: (Int, Int) -> Int = {
+    $0 + $1
+}
+
+
+// (Int, Double) -> Int
+
+let intDoubleClosure :(Int, Double) -> Int = {(i, di) -> Int in
+    return i
+}
+
+// ([String]) -> Int
+
+let arrayStringClosure: ([String]) -> Int = {(arrayString) -> Int in
+    arrayString.count
+}
+
+// (String, Int) -> [String: Int]
+
+let stringIntClosure: (String, Int) -> [String: Int] = {(txt, num) -> [String: Int] in
+    return [txt:num]
+}
