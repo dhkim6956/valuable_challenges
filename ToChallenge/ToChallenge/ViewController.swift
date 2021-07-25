3ab4c738096633687367f7e031f406a64403663d
YE JIN LEE <84134902+yeah-zin@users.noreply.github.com>
2021-07-25 22:56:57 +0900

    new Proj

---
+++ ToChallenge/ToChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToChallenge
+//
+//  Created by Yejin Lee on 2021/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
