b430ad782e93e29b2434d2f3780dfb6382af888c
YE JIN LEE <84134902+yeah-zin@users.noreply.github.com>
2021-07-25 16:46:02 +0900

    fix

--- sandbox/yeah-zin/ClosureTest.playground/Contents.swift
+++ sandbox/yeah-zin/ClosureTest.playground/Contents.swift
@@ -1,5 +1,6 @@
 // 문제1
 // 3의 배수를 찾고 +1을 하여 배열에 넣으세요.
+// filter, map
 
 let array = [1,2,3,4,5,6,7,8,9]
 var retArray: [Int] = []
@@ -15,6 +16,7 @@ print(retArray)
 
 // 문제2
 // 앞에 만들어진 결과 array(retArray)의 모든 수 합을 구하세요.
+// reduce
 
 var ret = 0
 
