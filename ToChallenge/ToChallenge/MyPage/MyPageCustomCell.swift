d015a48fd22855b254b16d20618874a0d85179b2
wangjiwon <wldnjs010203@gmail.com>
2021-08-09 00:44:51 +0900

    MyPageView

---
+++ ToChallenge/ToChallenge/MyPage/MyPageCustomCell.swift
@@ -0,0 +1,33 @@
+//
+//  MyPageCustomCell.swift
+//  ToChallenge
+//
+//  Created by 왕지원 on 2021/08/09.
+//
+
+import Foundation
+import UIKit
+
+class MyPageCustomCell: UITableViewCell {
+    
+    @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var startLevel: UILabel!
+    @IBOutlet weak var finisedLevel: UILabel!
+    @IBOutlet weak var presentPointLabel: UILabel!
+    @IBOutlet weak var finishedPointLabel: UILabel!
+    @IBOutlet weak var ongoinChallengeCount: UILabel!
+    @IBOutlet weak var finishedChallengeCount: UILabel!
+    @IBOutlet weak var failedChallengeCount: UILabel!
+    @IBOutlet weak var pointProgressView: UIProgressView!
+    
+    
+    
+
+    
+
+    
+
+    
+    
+    
+}
--- ToChallenge/ToChallenge/MyPage/MyPageViewController.swift
+++ ToChallenge/ToChallenge/MyPage/MyPageViewController.swift
@@ -5,25 +5,100 @@
 //  Created by Yejin Lee on 2021/08/06.
 //
 
+import Foundation
 import UIKit
 
-class MyPageViewController: UIViewController {
-
+class MyPageTableView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var myPageTableView: UITableView!
+    
+    let sectionMenuArray = ["내 프로필", "로그인 정보", "나의 도전", "앱 정보"]
+    let firstMenuArray = ["연결 ID"]
+    let secondMenuArray = ["관심 분야 설정", "포인트 교환"]
+    let thirdMenuArray = ["개인 정보 정책", "이용 약관"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        myPageTableView.backgroundColor = #colorLiteral(red: 0.9720780253, green: 0.9662989974, blue: 0.9765200019, alpha: 1)
+        myPageTableView.dataSource = self
+        myPageTableView.delegate = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return firstMenuArray.count
+        case 2:
+            return secondMenuArray.count
+        case 3:
+            return thirdMenuArray.count
+        default:
+            return 0
+        }
     }
-    */
-
-}
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        if indexPath.section == 0 {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyPageCustomCell", for: indexPath) as! MyPageCustomCell
+            
+            cell.characterImage = .none
+            cell.startLevel?.text = "Lv 4"
+            cell.finisedLevel?.text = "Lv 5"
+            cell.presentPointLabel?.text = "82pts"
+            cell.finishedPointLabel?.text = "500pts"
+            cell.ongoinChallengeCount?.text = "5"
+            cell.finishedChallengeCount?.text = "7"
+            cell.failedChallengeCount?.text = "1"
+            cell.pointProgressView?.progress = (Float(Float(82) / Float(500)))
+            
+            cell.backgroundColor = #colorLiteral(red: 0.9720780253, green: 0.9662989974, blue: 0.9765200019, alpha: 1)
+            
+            cell.pointProgressView.progressTintColor = #colorLiteral(red: 0.2304375768, green: 1, blue: 0.3039473295, alpha: 1)
+            cell.pointProgressView.trackTintColor = .darkGray
+            
+            return cell
+    
+        }  else if indexPath.section == 1 {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "MyPageCell")
+            
+            cell.textLabel?.text = firstMenuArray[indexPath.row]
+            
+            return cell
+            
+        } else if indexPath.section == 2 {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "MyPageCell")
+            cell.textLabel?.text = secondMenuArray[indexPath.row]
+            
+            return cell
+            
+        } else {
+            
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "MyPageCell")
+            
+            cell.textLabel?.text = thirdMenuArray[indexPath.row]
+            
+            return cell
+            
+        }
+    
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionMenuArray.count
+    }
+   
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionMenuArray[section]
+    }
+    
+    
+    
+    }
