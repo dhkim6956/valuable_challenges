5e7664501e3d0d617cdf093249406fd966481620
YE JIN LEE <84134902+yeah-zin@users.noreply.github.com>
2021-08-12 16:48:34 +0900

    fix cell

--- ToChallenge/ToChallenge/Achievements/AchievementsViewController.swift
+++ ToChallenge/ToChallenge/Achievements/AchievementsViewController.swift
@@ -17,31 +17,34 @@ class AchievementsViewController: UIViewController {
 
         collectView.dataSource = self
         collectView.delegate = self
+        
     }
 
 }
 
 extension AchievementsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return collectModel.arrayList.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return collectModel.arrayList[1].collectList.count
-    }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "CollectCell", for: indexPath) as! CollectCell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectCell", for: indexPath) as? CollectCell
+        else {
+            return UICollectionViewCell()
+        }
         
+        cell.collectImage.image = UIImage(named: collectModel.arrayList[indexPath.row].imgTitle)
+        cell.collectLabel.text = collectModel.arrayList[indexPath.row].title
         
-        cell.collectImage?.image = UIImage(named: "charactor6")
-        cell.collectLabel?.text = collectModel.arrayList[1].collectList[indexPath.row]
+
+        cell.layer.cornerRadius = 10
+        cell.layer.masksToBounds = true
+        cell.layer.shadowColor = UIColor.lightGray.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        cell.layer.shadowRadius = 6.0
+        cell.layer.shadowOpacity = 1.0
         
         return cell
-        
     }
     
     
--- ToChallenge/ToChallenge/Achievements/achievementsModel.swift
+++ ToChallenge/ToChallenge/Achievements/achievementsModel.swift
@@ -6,17 +6,29 @@
 //
 
 import Foundation
+import UIKit
 
 struct Collectibles {
-    let sectionTitle : String
-    let collectList : [String]
+    let title: String
+    let imgTitle: String
+    var image: UIImage? {
+        return UIImage(named: "\(imgTitle)")
+    }
+    init(title: String, imgTitle: String) {
+        self.title = title
+        self.imgTitle = imgTitle
+    }
 }
 
 
 class AchivementsModel {
     var arrayList: [Collectibles] = []
     init() {
-        arrayList.append(Collectibles(sectionTitle: "레벨 달성", collectList: ["Level 1","Level 2","Level 3","Level 4","Level 5","Level 6"]))
-        arrayList.append(Collectibles(sectionTitle: "독특한 경험", collectList: ["매번 실패", "하루도 못가"]))
+        arrayList.append(Collectibles(title: "Level1", imgTitle: "charactor1"))
+        arrayList.append(Collectibles(title: "Level2", imgTitle: "charactor2"))
+        arrayList.append(Collectibles(title: "Level3", imgTitle: "charactor3"))
+        arrayList.append(Collectibles(title: "Level4", imgTitle: "charactor4"))
+        arrayList.append(Collectibles(title: "Level5", imgTitle: "charactor5"))
+        arrayList.append(Collectibles(title: "Level6", imgTitle: "charactor6"))
     }
 }
