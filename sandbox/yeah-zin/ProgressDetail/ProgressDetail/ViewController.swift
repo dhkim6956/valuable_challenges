3be863f4a64460ce5bd8c06db14ff3702148a346
YE JIN LEE <84134902+yeah-zin@users.noreply.github.com>
2021-08-27 15:38:28 +0900

    sandbox

--- sandbox/yeah-zin/ProgressDetail/ProgressDetail/ViewController.swift
+++ sandbox/yeah-zin/ProgressDetail/ProgressDetail/ViewController.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import FSCalendar
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance{
+    
+    @IBOutlet var calendar: FSCalendar!
+    
+    var formatter = DateFormatter()
+    
+    let nums: [Int] = [1,2,3,4]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calendar.delegate = self
+        calendar.dataSource = self
+        
+        // calendar.scrollDirection = .horizontal
+        // calendar.scope = .month
+        calendar.appearance.titleFont = UIFont.systemFont(ofSize: 13)
+        calendar.appearance.headerTitleFont = UIFont.boldSystemFont(ofSize: 16)
+        calendar.appearance.weekdayFont = UIFont.boldSystemFont(ofSize: 14)
+        
+        calendar.appearance.todayColor = .systemGreen
+        calendar.appearance.titleTodayColor = .white
+        // calendar.appearance.headerTitleColor = .systemRed
+        // calendar.appearance.weekdayTextColor = .systemRed
+        
+        calendar.appearance.headerMinimumDissolvedAlpha = 0.0
+        
+        calendar.allowsMultipleSelection = true
+
+
     }
+    
 
 
 }
